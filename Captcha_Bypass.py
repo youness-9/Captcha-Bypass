59b37db5b3925ea48e68568640bdccd7e54ef013
youness <152264278+youness-9@users.noreply.github.com>
2024-07-20 08:58:12 +0100

    0.0.1

---
+++ Captcha_Bypass.py
@@ -0,0 +1,64 @@
+import requests
+import re
+import urllib.parse
+def urls():
+    with open("cap.txt") as f:
+        for line in f:
+            vv=line.rstrip()
+            nextline = next(f)
+            cc=nextline.rstrip()
+            return vv,cc
+def k():
+    a=urllib.parse.urlparse(urls()[1])
+    vv=a.query
+    cc=vv.split("=")
+    return cc
+    
+def bypass_recaptcha():
+    session = requests.Session()
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
+        "Accept": "*/*",
+        "Accept-Language": "fa,en;q=0.9,en-GB;q=0.8,en-US;q=0.7",
+        "Connection": "keep-alive",
+        "Origin": "https://www.google.com",
+        "Referer": urls()[0],
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    anchor_url = urls()[0]
+    anchor_response = session.get(anchor_url, headers=headers)
+    
+    # Extract the recaptcha token from the anchor response
+    recaptcha_token = re.search(r'"recaptcha-token" value="(.*?)"', anchor_response.text).group(1)    
+    # Second request to reload the recaptcha
+    reload_url = urls()[1]
+    post_data = {
+        "v": "-80zvSY9h4i8O-ocN2P5qTJk",
+        "reason": "q",
+        "c": recaptcha_token,
+        "k": k(),
+        "co": "aHR0cHM6Ly9hY2NvdW50LmJodnIuY29tOjQ0Mw..",
+        "hl": "en",
+        "size": "invisible",
+        "chr": "[89,64,27]",
+        "vh": "13599012192",
+        "bg": "!q62grYxHRvVxjUIjSFNd0mlvrZ-iCgIHAAAB6FcAAAANnAkBySdqTJGFRK7SirleWAwPVhv9-XwP8ugGSTJJgQ46-0IMBKN8HUnfPqm4sCefwxOOEURND35prc9DJYG0pbmg_jD18qC0c-lQzuPsOtUhHTtfv3--SVCcRvJWZ0V3cia65HGfUys0e1K-IZoArlxM9qZfUMXJKAFuWqZiBn-Qi8VnDqI2rRnAQcIB8Wra6xWzmFbRR2NZqF7lDPKZ0_SZBEc99_49j07ISW4X65sMHL139EARIOipdsj5js5JyM19a2TCZJtAu4XL1h0ZLfomM8KDHkcl_b0L-jW9cvAe2K2uQXKRPzruAvtjdhMdODzVWU5VawKhpmi2NCKAiCRUlJW5lToYkR_X-07AqFLY6qi4ZbJ_sSrD7fCNNYFKmLfAaxPwPmp5Dgei7KKvEQmeUEZwTQAS1p2gaBmt6SCOgId3QBfF_robIkJMcXFzj7R0G-s8rwGUSc8EQzT_DCe9SZsJyobu3Ps0-YK-W3MPWk6a69o618zPSIIQtSCor9w_oUYTLiptaBAEY03NWINhc1mmiYu2Yz5apkW_KbAp3HD3G0bhzcCIYZOGZxyJ44HdGsCJ-7ZFTcEAUST-aLbS-YN1AyuC7ClFO86CMICVDg6aIDyCJyIcaJXiN-bN5xQD_NixaXatJy9Mx1XEnU4Q7E_KISDJfKUhDktK5LMqBJa-x1EIOcY99E-eyry7crf3-Hax3Uj-e-euzRwLxn2VB1Uki8nqJQVYUgcjlVXQhj1X7tx4jzUb0yB1TPU9uMBtZLRvMCRKvFdnn77HgYs5bwOo2mRECiFButgigKXaaJup6NM4KRUevhaDtnD6aJ8ZWQZTXz_OJ74a_OvPK9eD1_5pTG2tUyYNSyz-alhvHdMt5_MAdI3op4ZmcvBQBV9VC2JLjphDuTW8eW_nuK9hN17zin6vjEL8YIm_MekB_dIUK3T1Nbyqmyzigy-Lg8tRL6jSinzdwOTc9hS5SCsPjMeiblc65aJC8AKmA5i80f-6Eg4BT305UeXKI3QwhI3ZJyyQAJTata41FoOXl3EF9Pyy8diYFK2G-CS8lxEpV7jcRYduz4tEPeCpBxU4O_KtM2iv4STkwO4Z_-c-fMLlYu9H7jiFnk6Yh8XlPE__3q0FHIBFf15zVSZ3qroshYiHBMxM5BVQBOExbjoEdYKx4-m9c23K3suA2sCkxHytptG-6yhHJR3EyWwSRTY7OpX_yvhbFri0vgchw7U6ujyoXeCXS9N4oOoGYpS5OyFyRPLxJH7yjXOG2Play5HJ91LL6J6qg1iY8MIq9XQtiVZHadVpZVlz3iKcX4vXcQ3rv_qQwhntObGXPAGJWEel5OiJ1App7mWy961q3mPg9aDEp9VLKU5yDDw1xf6tOFMwg2Q-PNDaKXAyP_FOkxOjnu8dPhuKGut6cJr449BKDwbnA9BOomcVSztEzHGU6HPXXyNdZbfA6D12f5lWxX2B_pobw3a1gFLnO6mWaNRuK1zfzZcfGTYMATf6d7sj9RcKNS230XPHWGaMlLmNxsgXkEN7a9PwsSVwcKdHg_HU4vYdRX6vkEauOIwVPs4dS7yZXmtvbDaX1zOU4ZYWg0T42sT3nIIl9M2EeFS5Rqms_YzNp8J-YtRz1h5RhtTTNcA5jX4N-xDEVx-vD36bZVzfoMSL2k85PKv7pQGLH-0a3DsR0pePCTBWNORK0g_RZCU_H898-nT1syGzNKWGoPCstWPRvpL9cnHRPM1ZKemRn0nPVm9Bgo0ksuUijgXc5yyrf5K49UU2J5JgFYpSp7aMGOU"
+    }
+
+    reload_response = session.post(reload_url, headers=headers, data=post_data)
+    #print("Successfully accessed reload URL")
+    #print("Response text:", reload_response.text)
+
+    # Checking for the token in the response
+    try:
+        token = reload_response.text.split('rresp","')[1].split('"')[0]
+        return token
+    except:
+        pass
+        
+print(bypass_recaptcha())
+
